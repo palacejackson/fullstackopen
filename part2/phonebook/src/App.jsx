f0f6e76c525cef6e3f7292c4d569ddb9fe3f340e
Alice Jackson <alicejj92@gmail.com>
2023-11-20 12:45:07 +0000

    exercise 2.8 complete

--- part2/phonebook/src/App.jsx
+++ part2/phonebook/src/App.jsx
@@ -1,35 +1,46 @@
 import { useLayoutEffect, useState } from 'react'
 
-const Person = ({ name }) => {
+const Person = ({ name, number }) => {
   return (
-    <li>{name}</li>
+    <li>{name} {number}</li>
   )
 }
 
 const App = () => {
   const [persons, setPersons] = useState([
-    { name: 'Arto Hellas' }
+    { name: 'Arto Hellas', number: '07745646169'}
   ])
   const [newName, setNewName] = useState('')
+  const [newNumber, setNewNumber] = useState('')
 
   const addName = (e) => {
     e.preventDefault()
-    const nameObject =  {
+    const personObject =  {
       name: newName,
+      number: newNumber,
       id: persons.length + 1
     }
+
     if (persons.some((person) => person.name === newName)) {
       alert(`${newName} is already added to phonebook`)
       setNewName('')
       return
     }
-    setPersons(persons.concat(nameObject))
+
+    setPersons(persons.concat(personObject))
     setNewName('')
+    setNewNumber('')
   }
 
   const handleAddName = (e) => {
     setNewName(e.target.value)
   }
+
+  const handleAddNumber= (e) => {
+    setNewNumber(e.target.value)
+  }
+
+
 
   return (
     <div>
@@ -41,13 +52,19 @@ const App = () => {
             onChange={handleAddName} />
         </div>
         <div>
+          number: <input
+            value={newNumber}
+            onChange={handleAddNumber}
+            />
+        </div>
+        <div>
           <button type="submit">add</button>
         </div>
       </form>
       <h2>Numbers</h2>
       <ul>
         {persons.map(person =>
-          <Person key={person.id} name={person.name} />
+          <Person key={person.id} name={person.name} number={person.number} />
         )}
       </ul>
     </div>
