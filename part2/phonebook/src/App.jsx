f7e5b6e84641a4e810cbbfaca35e66e487679d49
Alice Jackson <alicejj92@gmail.com>
2023-11-20 16:30:41 +0000

    exercise 2.9 complete

--- part2/phonebook/src/App.jsx
+++ part2/phonebook/src/App.jsx
@@ -2,16 +2,20 @@ import { useLayoutEffect, useState } from 'react'
 
 const Person = ({ name, number }) => {
   return (
-    <li>{name} {number}</li>
+    <div>{name} {number}</div>
   )
 }
 
 const App = () => {
   const [persons, setPersons] = useState([
-    { name: 'Arto Hellas', number: '07745646169'}
+    { name: 'Arto Hellas', number: '040-123456', id: 1 },
+    { name: 'Ada Lovelace', number: '39-44-5323523', id: 2 },
+    { name: 'Dan Abramov', number: '12-43-234345', id: 3 },
+    { name: 'Mary Poppendieck', number: '39-23-6423122', id: 4 }
   ])
   const [newName, setNewName] = useState('')
   const [newNumber, setNewNumber] = useState('')
+  const [searchValue, setSearchValue] = useState('')
 
   const addName = (e) => {
     e.preventDefault()
@@ -40,11 +44,25 @@ const App = () => {
     setNewNumber(e.target.value)
   }
 
+  const handleSearch = (e) => {
+    setSearchValue(e.target.value)
+  }
 
+  const searchPersons = () => {
+    if (!searchValue) return persons
+    return persons.filter(person => person.name.includes(searchValue))
+  }
 
   return (
     <div>
       <h2>Phonebook</h2>
+      <div>Search:
+        <input
+          value={searchValue}
+          onChange={handleSearch}
+        />
+      </div>
+      <h2>Add new contact</h2>
       <form onSubmit={addName}>
         <div>
           name: <input
@@ -62,11 +80,11 @@ const App = () => {
         </div>
       </form>
       <h2>Numbers</h2>
-      <ul>
-        {persons.map(person =>
+      <>
+        {searchPersons().map(person =>
           <Person key={person.id} name={person.name} number={person.number} />
         )}
-      </ul>
+       </>
     </div>
   )
 }
