289f3a30352198cd599f560cbd0d272ecc6da7f2
Alice Jackson <alicejj92@gmail.com>
2023-12-18 19:23:30 +0000

    modified gitignore

--- part2/courseinfo/src/components/Course.jsx
+++ part2/courseinfo/src/components/Course.jsx
@@ -10,22 +10,21 @@ const Total = ({ parts }) =>  {
 const Part = ({ part }) => <p> {part.name} {part.exercises} </p>
 
 const Content = ({ parts }) => {
+  console.log(parts)
   return (
     parts.map(part =>
-      <Part
-        key={part.id} part={part}
-      />
+      <Part key={part.id} part={part} />
     )
   )
 }
 
 const Course = ( {course} ) => {
   return (
-  <>
+  <div>
     <Header course={course.name} />
     <Content parts={course.parts} />
     <Total parts={course.parts} />
-  </>
+  </div>
   )
 }
 
